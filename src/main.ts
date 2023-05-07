99699426e9a6921c3a4a92969d506aa781da1ca0
andy010629 <andy010629@gmail.com>
2023-05-07 22:52:00 +0800

    add pwa support

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,12 @@ import App from './App.vue'
 
 const myApp = createApp(App)
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+
 myApp.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
 }).use(VueFire, {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true, // 生成源映射以支持 Workbox
+  },
   plugins: [
     vue({
       template: { transformAssetUrls }
@@ -12,6 +16,8 @@ export default defineConfig({
 
     quasar({
       sassVariables: 'src/quasar-variables.sass'
-    })
+    }),
+    VitePWA({ registerType: 'autoUpdate' })
+    
   ]
 })
