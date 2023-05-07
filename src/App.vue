39f2df6168c493f18f03fee9ddedc0895577d7b3
andy010629 <andy010629@gmail.com>
2023-05-07 21:04:45 +0800

    add count_total && fix css && clear and alert when adding

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,33 @@
     <q-page-container>
       <q-page class="column justify-start items-center q-py-xl">
         <q-btn label="新增紀錄" color="primary" size="lg" @click="openModal" />
+        <div class="text-h6 text-weight-bold q-py-md">累積幣量:
+          {{ count_total }}
+        </div>
          <q-table
 
-  :rows="tableData"
+  :rows="tableData.reverse()"
   :columns="columns"
   row-key="name"
   class="q-my-md"
-  style="width: 70vw;"
+  style="width: 90vw;"
 >
   <template v-slot:body="props">
     <q-tr :props="props">
       
-      <q-td v-for="col in props.cols" :key="col.name" :props="props">
+      <q-td v-for="(col,idx) in props.cols" :key="col.name" :props="props">
          <template v-if="col.name === 'delete'">
-        <q-btn flat color="negative" style="opacity:70%" icon="delete" @click="deleteRecord(props.row)" />
+        <q-btn flat color="negative" style="opacity:70%" icon="delete" @click="showDialog(idx)" />
       </template>
         {{ col.value }}
       </q-td>
     </q-tr>
   </template>
-</q-table>
+      </q-table>
         <q-dialog v-model="state.modalOpened">
-          <q-card class="q-px" style="width: 70vw;">
+          <q-card class="q-px" style="width: 85vw;">
             <q-card-section>
-              <div class="text-h6 text-bold">新增紀錄</div>
+            <div class="text-h6 text-bold">新增紀錄</div>
             </q-card-section>
             <q-card-section>
               <q-input
@@ -37,7 +40,7 @@
                 label="日期"
                 class="q-mb-md"
               />
-              <q-input outlined v-model="text" label="計數器" class="q-mb-md" />
+              <q-input type="number" outlined v-model="text" label="計數器" class="q-mb-md" />
               <div class="row q-py-sm justify-between items-center">
                 <div class="text-h7">出貨紀錄</div>
                 <q-btn
@@ -63,6 +66,21 @@
             </q-card-actions>
           </q-card>
         </q-dialog>
+
+          <q-dialog v-model="dialog" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">確認刪除</div>
+        </q-card-section>
+        <q-card-section>
+          您確定要刪除此項目嗎？
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="取消" color="primary" v-close-popup />
+          <q-btn flat label="刪除" color="negative" @click="confirmDelete" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
       </q-page>
       <!-- a table with data -->
       
@@ -89,22 +107,34 @@ const db = useFirestore()
 const columns = ref([
 
   // { name: "id", label: "ID", field: "id", sortable: true,  align: "left"},
-
+  {
+    name: "delete",
+    label: "删除",
+    align: "center"
+  
+  },
   { name: "date", label: "日期", field: "date", sortable: true,tableShow: true, align: "left" },
   { name: "text", label: "計數器", field: "text", sortable: true ,tableShow: true, align: "left"},
   { name: "items", label: "出貨紀錄", field: (row) => row.items.map((item) => item.name).join(', '), sortable: true  ,align: "left"},
-  {
-  name: "delete",
-  label: "删除",
-  align: "center"
  
-},
 ]);
 
 // const tableData = ref([]);
-const tableData = ref([
+const tableData = ref([]);
+const count_total = ref(0);
 
-]);
+const dialog = ref(false);
+let currentIndex;
+
+function showDialog(index) {
+  currentIndex = index;
+  dialog.value = true;
+}
+
+function confirmDelete() {
+  deleteRecord(tableData.value[currentIndex]);
+  dialog.value = false;
+}
 
 
 const tableDataLoaded = ref(false);
@@ -119,6 +149,7 @@ onSnapshot(collection(db, "record"), (snapshot) => {
   });
   tableData.value = newData;
   tableDataLoaded.value = true;
+  count_total.value = Math.max(...tableData.value.map((item) => item.text)) - Math.min(...tableData.value.map((item) => item.text))
 });
 
 
@@ -129,15 +160,23 @@ async function deleteRecord(record) {
   await deleteDoc(recordRef);
 }
 
+
 function insert() {
   const record_data = {
     date: date.value,
     text: text.value,
     items: inputs.value,
   }
-  addDoc(collection(db, "record"), record_data);
+  if(parseInt(text.value) < parseInt(tableData.value[tableData.value.length-1].text)){
+    alert("計數器應大於上一次紀錄")
+  }
+  // addDoc(collection(db, "record"), record_data);
   // close the modal
   state.modalOpened = false;
+  // reset the form
+  text.value = "";
+  date.value = new Date().toISOString().substr(0, 10);
+  inputs.value = [];
 }
 
 
